ebaaec6b454ee0435c6dafba6c6545278532f314
Anderson Buffon Berto <abuffon@gmail.com>
2021-11-17 10:43:51 -0300

    JUnit

--- algorithm.i/src/main/java/leetcode/algorithm/i/RottingOranges.java
+++ algorithm.i/src/main/java/leetcode/algorithm/i/RottingOranges.java
@@ -8,8 +8,9 @@ public class RottingOranges {
 		int[][] grid2 = new int[][] { {2,1,1}, {0,1,1}, {1,0,1}};//-1
 		int[][] grid3 = new int[][] { {0,2}};//0
 
-		PrintUtils.printMatrix(null);
-		
+		PrintUtils.printMatrix(grid1); 
+		PrintUtils.printMatrix(grid2);
+		PrintUtils.printMatrix(grid3);
 	}
 	
 	public static int orangesRotting(int[][] grid) {
